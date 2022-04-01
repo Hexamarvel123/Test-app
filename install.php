4f7896f105178d3ca031c5155163495b5e065f37
Hexamarvel123 <102784746+Hexamarvel123@users.noreply.github.com>
2022-04-01 11:58:50 +0530

    Update install.php

--- install.php
+++ install.php
@@ -2,9 +2,9 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "2dc88a9bb3bc47dc356c59dab69d116e";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "https://hexamarvel-app-test.myshopify.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
